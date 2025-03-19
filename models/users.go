3531d30d8b3a81a9a13c59865d8972b876002114
Ibrohim Mubarok <befousmain@gmail.com>
2025-03-19 14:17:34 +0700

    Tambah models session

--- models/users.go
+++ models/users.go
@@ -1,13 +1,25 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Users struct {
 	ID       *primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	Username string              `json:"username" bson:"username"`
 	Password string              `json:"password,omitempty" bson:"password,omitempty"`
-	Nama     string              `json:"nama" bson:"nama"`
-	No_Telp  string              `json:"no_telp" bson:"no_telp"`
-	Email    string              `json:"email" bson:"email"`
+	Nama     string              `json:"nama,omitempty" bson:"nama,omitempty"`
+	No_Telp  string              `json:"no_telp,omitempty" bson:"no_telp,omitempty"`
+	Email    string              `json:"email,omitempty" bson:"email,omitempty"`
 	Role     string              `json:"role" bson:"role"`
+}
+
+type Session struct {
+	ID         *primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Username   string              `json:"username" bson:"username"`
+	Token      string              `json:"token" bson:"token"`
+	User_Agent string              `json:"user_agent" bson:"user_agent"`
+	Expire_At  time.Time           `json:"expire_at" bson:"expire_at"`
 }
