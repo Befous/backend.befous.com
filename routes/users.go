ba80fd76aa22220eac9d54cf17df7a0f730c92f1
Ibrohim Mubarok <befousmain@gmail.com>
2025-03-19 14:18:16 +0700

    Tambah 2 path endpoint

--- routes/users.go
+++ routes/users.go
@@ -4,8 +4,11 @@ import (
 	"net/http"
 
 	"github.com/Befous/api.befous.com/controllers"
+	"github.com/Befous/api.befous.com/middleware"
 )
 
 func Route(router *http.ServeMux) {
-	router.HandleFunc("GET /", controllers.RootController)
+	router.HandleFunc("GET /", controllers.Root)
+	router.HandleFunc("POST /login", controllers.Login)
+	router.Handle("GET /session", middleware.IsAuthenticated(http.HandlerFunc(controllers.Session)))
 }
