130e34892fa2b61b7498876922ca405fde28b61f
Ibrohim Mubarok <befousmain@gmail.com>
2025-03-19 14:18:06 +0700

    Buat controller untuk login dan session

--- controllers/users.go
+++ controllers/users.go
@@ -3,22 +3,64 @@ package controllers
 import (
 	"net/http"
 
-	// "github.com/Befous/api.befous.com/helpers"
 	"github.com/Befous/api.befous.com/models"
 	"github.com/Befous/api.befous.com/utils"
-	// "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func RootController(w http.ResponseWriter, r *http.Request) {
-	// mconn := utils.SetConnection()
-	// users, err := helpers.GetAllDoc[[]models.Users](mconn, "users", primitive.M{})
-	// if err != nil {
-	// 	utils.WriteJSONResponse(w, http.StatusOK, models.Pesan{
-	// 		Message: "Error:" + err.Error(),
-	// 	})
-	// 	return
-	// }
+func Root(w http.ResponseWriter, r *http.Request) {
 	utils.WriteJSONResponse(w, http.StatusOK, models.Pesan{
 		Message: "Hello World",
+	})
+}
+
+func Session(w http.ResponseWriter, r *http.Request) {
+	var session models.Users
+	session, err := utils.DecodeJWT(r)
+	if err != nil {
+		utils.WriteJSONResponse(w, http.StatusBadRequest, models.Pesan{
+			Message: "Error decode token: " + err.Error(),
+		})
+		return
+	}
+
+	utils.WriteJSONResponse(w, http.StatusOK, models.Pesan{
+		Message: "Berikut data session anda",
+		Data:    session,
+	})
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	mconn := utils.SetConnection()
+	var user models.Users
+
+	utils.ParseBody(w, r, &user)
+
+	if !utils.UsernameExists(mconn, user) {
+		utils.WriteJSONResponse(w, http.StatusBadRequest, models.Pesan{
+			Message: "Akun tidak ditemukan",
+		})
+		return
+	}
+
+	if !utils.IsPasswordValid(mconn, user) {
+		utils.WriteJSONResponse(w, http.StatusBadRequest, models.Pesan{
+			Message: "Password salah",
+		})
+		return
+	}
+
+	userAgent := r.UserAgent()
+	token, err := utils.SignedJWT(mconn, user, userAgent)
+
+	if err != nil {
+		utils.WriteJSONResponse(w, http.StatusInternalServerError, models.Pesan{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	utils.WriteJSONResponse(w, http.StatusOK, models.Pesan{
+		Message: "Berhasil login",
+		Token:   token,
 	})
 }
