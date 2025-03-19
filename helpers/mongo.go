2585e671414bdde5ed813899f6271f23bdf3cc27
Ibrohim Mubarok <befousmain@gmail.com>
2025-03-19 14:17:22 +0700

    Tambah banyak function yang berhubungan dengan mongodb

--- helpers/mongo.go
+++ helpers/mongo.go
@@ -2,12 +2,18 @@ package helpers
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/Befous/api.befous.com/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type DataCount struct {
+	Total int64 `json:"total,omitempty"`
+}
 
 func MongoConnect(mconn models.DBInfo) (db *mongo.Database, err error) {
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mconn.DBString))
@@ -18,16 +24,169 @@ func MongoConnect(mconn models.DBInfo) (db *mongo.Database, err error) {
 	return
 }
 
-func GetAllDoc[T any](db *mongo.Database, collection string, filter bson.M) (doc T, err error) {
-	ctx := context.TODO()
+func InsertOneDoc(db *mongo.Database, collection string, doc interface{}) (insertedID interface{}, err error) {
+	insertResult, err := db.Collection(collection).InsertOne(context.TODO(), doc)
+	if err != nil {
+		fmt.Printf("InsertOneDoc: %v\n", err)
+		return nil, err
+	}
+	return insertResult.InsertedID, nil
+}
+
+func GetOneDoc[T any](db *mongo.Database, collection string, filter bson.M) (doc T) {
+	err := db.Collection(collection).FindOne(context.TODO(), filter).Decode(&doc)
+	if err != nil {
+		fmt.Printf("GetOneDoc: %v\n", err)
+	}
+	return
+}
+
+func GetOneLatestDoc[T any](db *mongo.Database, collection string, filter bson.M) (doc T) {
+	opts := options.FindOne().SetSort(bson.M{"$natural": -1})
+	err := db.Collection(collection).FindOne(context.TODO(), filter, opts).Decode(&doc)
+	if err != nil {
+		fmt.Printf("GetOneDoc: %v\n", err)
+	}
+	return doc
+}
+
+func GetAllDocByFilter[T any](db *mongo.Database, collection string, filter bson.M) (doc []T, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 	cur, err := db.Collection(collection).Find(ctx, filter)
 	if err != nil {
-		return
+		fmt.Printf("GetAllDoc: %v\n", err)
+		return nil, err
 	}
 	defer cur.Close(ctx)
 	err = cur.All(ctx, &doc)
 	if err != nil {
-		return
+		fmt.Printf("GetAllDoc Cursor Err: %v\n", err)
+		return nil, err
+	}
+	return doc, nil
+}
+
+func GetAllDocByFilterWithPagination[T any](db *mongo.Database, collection string, page, limit int, filter bson.M) (doc []T, count int64, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	findOptions := options.Find()
+	findOptions.SetSkip(int64((page - 1) * limit))
+	findOptions.SetLimit(int64(limit))
+	cur, err := db.Collection(collection).Find(ctx, filter, findOptions)
+	if err != nil {
+		fmt.Printf("GetAllDoc: %v\n", err)
+		return nil, 0, err
+	}
+	defer cur.Close(ctx)
+	err = cur.All(ctx, &doc)
+	if err != nil {
+		fmt.Printf("GetAllDoc Cursor Err: %v\n", err)
+		return nil, 0, err
+	}
+	count, err = db.Collection(collection).CountDocuments(ctx, filter)
+	if err != nil {
+		fmt.Printf("GetAllDoc CountDocuments Error: %v\n", err)
+		return nil, 0, err
+	}
+	return doc, count, nil
+}
+
+func GetAllDoc[T any](db *mongo.Database, collection string) (doc []T, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	cur, err := db.Collection(collection).Find(ctx, bson.M{})
+	if err != nil {
+		fmt.Printf("GetAllDoc: %v\n", err)
+		return nil, err
+	}
+	defer cur.Close(ctx)
+	err = cur.All(ctx, &doc)
+	if err != nil {
+		fmt.Printf("GetAllDoc Cursor Err: %v\n", err)
+		return nil, err
+	}
+	return doc, nil
+}
+
+func GetAllDocWithPagination[T any](db *mongo.Database, collection string, page, limit int) (doc []T, count int64, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	findOptions := options.Find()
+	findOptions.SetSkip(int64((page - 1) * limit))
+	findOptions.SetLimit(int64(limit))
+	cur, err := db.Collection(collection).Find(ctx, bson.M{}, findOptions)
+	if err != nil {
+		fmt.Printf("GetAllDoc: %v\n", err)
+		return nil, 0, err
+	}
+	defer cur.Close(ctx)
+	err = cur.All(ctx, &doc)
+	if err != nil {
+		fmt.Printf("GetAllDoc Cursor Err: %v\n", err)
+		return nil, 0, err
+	}
+	count, err = db.Collection(collection).CountDocuments(ctx, bson.M{})
+	if err != nil {
+		fmt.Printf("GetAllDoc CountDocuments Error: %v\n", err)
+		return nil, 0, err
+	}
+	return doc, count, nil
+}
+
+func GetAllDistinctDoc(db *mongo.Database, filter bson.M, fieldname, collection string) (doc []any) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	doc, err := db.Collection(collection).Distinct(ctx, fieldname, filter)
+	if err != nil {
+		fmt.Printf("GetAllDistinctDoc: %v\n", err)
 	}
 	return
+}
+
+func ReplaceOneDoc(db *mongo.Database, collection string, filter bson.M, doc interface{}) (updatereseult *mongo.UpdateResult) {
+	updatereseult, err := db.Collection(collection).ReplaceOne(context.TODO(), filter, doc)
+	if err != nil {
+		fmt.Printf("ReplaceOneDoc: %v\n", err)
+	}
+	return
+}
+
+func DeleteOneDoc(db *mongo.Database, collection string, filter bson.M) (result *mongo.DeleteResult) {
+	result, err := db.Collection(collection).DeleteOne(context.TODO(), filter)
+	if err != nil {
+		fmt.Printf("DeleteOneDoc: %v\n", err)
+	}
+	return
+}
+
+func DeleteDoc(db *mongo.Database, collection string, filter bson.M) (result *mongo.DeleteResult) {
+	result, err := db.Collection(collection).DeleteMany(context.TODO(), filter)
+	if err != nil {
+		fmt.Printf("DeleteDoc : %v\n", err)
+	}
+	return
+}
+
+func GetRandomDoc[T any](db *mongo.Database, collection string, size uint) (result []T, err error) {
+	filter := mongo.Pipeline{
+		{{Key: "$sample", Value: bson.D{{Key: "size", Value: size}}}},
+	}
+	ctx := context.Background()
+	cur, err := db.Collection(collection).Aggregate(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+	defer cur.Close(ctx)
+
+	if err = cur.All(ctx, &result); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func DocExists[T any](db *mongo.Database, collname string, filter bson.M, doc T) (result bool) {
+	err := db.Collection(collname).FindOne(context.Background(), filter).Decode(&doc)
+	return err == nil
 }
