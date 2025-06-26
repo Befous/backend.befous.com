d45c351046c7543f6b1208d51e4be980ce905c49
Ibrohim Mubarok <befousmain@gmail.com>
2025-06-26 12:38:20 +0700

    URL

--- routes/users.go
+++ routes/users.go
@@ -8,7 +8,10 @@ import (
 )
 
 func Route(router *http.ServeMux) {
-	router.HandleFunc("GET /", controllers.Root)
+	router.HandleFunc("GET /email/{email}", controllers.KirimGmail)
+	router.HandleFunc("POST /image", controllers.UploadImage)
+	router.HandleFunc("DELETE /image", controllers.DeleteImage)
+	router.HandleFunc("GET /", controllers.IpapiProxyHandler)
 	router.HandleFunc("POST /login", controllers.Login)
 	router.Handle("GET /session", middleware.IsAuthenticated(http.HandlerFunc(controllers.Session)))
 	router.Handle("GET /mangadex/cover/{id_manga}", middleware.IsAuthenticated(http.HandlerFunc(controllers.CoverMangadex)))
